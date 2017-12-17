9eb24f47bee59f64626878feab816796de3c2767
jsmyth4744 <jsmyth4744@gmail.com>
2017-12-16 17:26:05 -0800

    add | bellman-ford

--- src/REPL.java
+++ src/REPL.java
@@ -1,17 +1,10 @@
-import java.text.CharacterIterator;
-import java.text.StringCharacterIterator;
-import java.util.*;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
-
+import java.util.LinkedList;
 
 public class REPL {
 
     public static void main(String[] args) {
+
+        LinkedList<String> list = new LinkedList<>();
 
 
 
